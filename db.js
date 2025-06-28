2048cafe80b6b41339b76fb03fd30b76dea8f80e
Uday Bhatnagar <udaybhatnagar502@gmail.com>
2025-06-28 11:08:14 +0530

    databased implemted

---
+++ db.js
@@ -0,0 +1,44 @@
+const mongoose=require("mongoose");
+mongoose.connect(""); //replace with the original db link
+const schema=mongoose.Schema;
+const ObjectId=mongoose.Types.ObjectId;
+
+const UserSchema=new schema({
+    firstname:String,
+    lastname:String,
+    email:{type:String ,unique:true},
+    password:String
+})
+
+const ADminSchema=new schema({
+    firstname:String,
+    lastname:String,
+    email:{type:String ,unique:true},
+    password:String
+})
+
+const courseSchema=new schema({
+    title:String,
+    description:String,
+    price:Number,
+    imageurl:String,
+    CourseId:ObjectId
+})
+
+const purchaseSChema=new schema({
+    userId:ObjectId,
+    courseId:ObjectId,
+})
+
+const UserModel=mongoose.model("user",UserSchema);
+const AdminModel=mongoose.model("admin",ADminSchema);
+const CourseModel=mongoose.model("course",courseSchema);
+const PurchaseModel=mongoose.model("purchases",purchaseSChema);
+
+module.exports={
+    UserModel,
+    AdminModel,
+    PurchaseModel,
+    CourseModel,
+
+}
